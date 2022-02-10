183fb247451f85784f95df7cf22d0e08aadcf56d
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2022-02-11 00:41:58 +0530

    Updated Initial Setup

---
+++ iNews/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewsListTableViewController.swift
+//  iNews
+//
+//  Created by Ashish Langhe on 2/11/22.
+//
+
+import Foundation
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+    
+}
