50ec7f278f18e14f633ef30be2bb787185e28425
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2022-02-11 18:51:29 +0530

    Added ArticleViewModel

--- iNews/Services/WebServices.swift
+++ iNews/Services/WebServices.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class WebServices {
     
-    func getArticles(url: URL, completion: @escaping ([Any]?) -> ()) {
+    func getArticles(url: URL, completion: @escaping ([Article]?) -> ()) {
         
         let task = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
             
@@ -18,7 +18,13 @@ class WebServices {
                 print(error.localizedDescription)
                 completion(nil)
             } else if let data = data {
-                print(data)
+                
+                let articleList = try? JSONDecoder().decode(ArticleList.self, from: data)
+                
+                if let articleList = articleList {
+                    completion(articleList.articles)
+                }
+            
             }
         })
         
